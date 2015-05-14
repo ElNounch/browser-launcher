1711264a2afe8764df6031fc7380bfcfc7cbd710
ElNounch <ElNounch@users.noreply.github.com>
2015-05-14 02:07:29 +0200

    Distinct screenshots 2

--- test/base.js
+++ test/base.js
@@ -48,7 +48,7 @@ function checkLauncher( t, launch, browser ) {
       res.end()
       t.pass('javascript worked')
       job_done = true
-      require('child_process').spawnSync( 'screenshot', [ 'screenshots/' + browser + '.png' ] )
+      require('child_process').spawnSync( 'screenshot', [ 'screenshots\\' + browser + '.png' ] )
       clearTimeout( timer )
       proc.kill('SIGTERM')
       server.close()
